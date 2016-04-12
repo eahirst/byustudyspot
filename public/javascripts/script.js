06dd4da2cd440d38f6349c934362dfd0bdd6ebbb
eahirst <elenahirst42@gmail.com>
2016-04-12 09:32:06 -0600

    Update script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,10 +7,7 @@ $scope.tabs = [{
         }, {
             title: 'Add a Study Spot',
             url: 'two.tpl.html'
-        }, {
-            title: 'Login',
-            url: 'three.tpl.html'
-    }];
+        }];
  $scope.currentTab = 'one.tpl.html';
 
     $scope.onClickTab = function (tab) {
@@ -23,10 +20,47 @@ $scope.tabs = [{
   var ref = new Firebase("https://study-spot-finder.firebaseio.com/");
   $scope.buildings = $firebaseArray(ref);
   $scope.update = function(newSpot) {
-	  var mySpot = {from:newSpot.name || "",building:newSpot.building,floor:newSpot.floor,wifi:newSpot.wifi,cell:newSpot.cell,light:newSpot.light};
+	  var mySpot = {building:newSpot.building,floor:newSpot.floor,wifi:newSpot.wifi | 'false',cell:newSpot.cell | 'false',light:newSpot.light | 'false'};
       console.log(mySpot);
 	  $scope.buildings.$add(mySpot);
 	  newSpot.chat = "";
   }
+
+  $scope.search = function(searchSpot){
+		$scope.found = [];
+		console.log("testing testing");
+		console.log(searchSpot);
+		for(var i=0;i<$scope.buildings.length;i++){
+			if (searchSpot == null)
+			{
+				$scope.found.push($scope.buildings[i]);
+			}
+			else if(searchSpot.building == null || searchSpot.building == "" || $scope.buildings[i].building == searchSpot.building)
+			{
+				console.log("building found");
+				if(searchSpot.floor == null || searchSpot.floor == "" || $scope.buildings[i].floor == searchSpot.floor)
+				{
+console.log("floor found");
+
+					if($scope.buildings[i].wifi == searchSpot.wifi)
+					{
+console.log("wifi found");
+
+						if($scope.buildings[i].cell == searchSpot.cell)
+						{
+console.log("cell found");
+
+							if($scope.buildings[i].light == searchSpot.light)
+							{
+console.log("light found");
+
+								$scope.found.push($scope.buildings[i]);
+							}		
+						}
+					}
+				}
+			}
+		}
+	}
 }
 ]);
